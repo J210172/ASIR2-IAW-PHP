46ac01f73fc363356b7d0506575012c4a8d5a05d
210172 <210172@iessierradeguara.com>
2023-09-15 21:06:04 +0200

    Clase viernes 15/09/2023

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $root_dir = $_SERVER['DOCUMENT_ROOT'];
+    $ejer_dir = $root_dir . "/ejercicios";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios IAW</title>
+</head>
+<body>
+
+</body>
+</html>
