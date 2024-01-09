41b0a12c5a2e500bea5b48539c99a8f8828b798c
J210172 <210172@iessierradeguara.com>
2024-01-09 19:51:46 +0100

    Clase 09/01/2024

--- ejercicios/ejer09/index.php
+++ ejercicios/ejer09/index.php
@@ -88,26 +88,37 @@
             <td>{{id}}</td>
             <td>{{name}}</td>
             <td>{{alb_num}}</td>
-            <td>{{is-aragones}}</td>
-            <td><button class="btn primary-outline open-edit-form">Edit</button> Add</td>
+            <td class="{{bool-class}}">{{is_aragones}}</td>
+            <td>
+                <ul class="flex x-center">
+                    <li><button class="btn icon-only inline open-edit-form"><i class="icon md primary nf-fa-edit"></i></button></li>
+                    <li><button class="btn icon-only inline"><i class="icon md danger nf-fa-trash_o"></i></button></li>
+                </ul>
+            </td>
         </tr>
     </template>
-
+    
+    <header class="flex space-evenly">
+        <h1>Modal page</h1>
+        <ul class="flex center">
+            <button id="add-row" class="p-0 btn circle"><i class="icon lg success nf-fa-plus"></i></button>
+            <button id="refresh-table" class="p-0 btn circle"><i class="icon lg primary nf-fa-refresh"></i></button>
+            <button id="user" class="btn circle"><i class="icon lg secondary nf-fa-user"></i></button>
+        </ul>
+    </header>
     <div class="container">
-        <h1>Modal page <button><i class="nf nf-fa-edit"></i></button></h1>
         <div class="box">
             <table id="users-table" class="w-100">
                 <thead>
                     <tr>
-                        <th>Header 1</th>
-                        <th>Header 2</th>
-                        <th>Header 3</th>
-                        <th>Header 4</th>
-                        <th>Options</th>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Numb Albs</th>
+                        <th>Aragones</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
                 </tbody>
             </table>
         </div>
@@ -129,10 +140,24 @@
            let modal_body = $('template#artist-form').html();
            open_modal('Edit', modal_body);
         });
+
+        $('#refresh-table').on('click', function() {
+            let table = $('table#users-table tbody');
+            let tmp_row = $('template#user-row').html();
+
+            tmp_row=tmp_row.replace('{{id}}', '69');
+            tmp_row=tmp_row.replace('{{name}}', 'HoHoHooo!!');
+            tmp_row=tmp_row.replace('{{alb_num}}', '3');
+            tmp_row=tmp_row.replace('{{is_aragones}}', 'Si');
+            tmp_row=tmp_row.replace('{{bool-class}}', 'bien');
+
+            table.append(tmp_row);
+        });
+        /*
         $(function() {
             let modal_body = $('template#artist-form').html();
             open_modal('Test', modal_body);     
-        })
+        })*/
     </script>
 </body>
 </html>
